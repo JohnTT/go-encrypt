a358a0544310b5f0f0adea4b28bba220d2d2b6b9
John Chen <jmc_tt@live.com>
2025-06-01 20:31:54 -0400

    Delete doc.go

--- symmetric/doc.go
+++
@@ -1,4 +0,0 @@
-// Package symmetric provides utilities for symmetric encryption and decryption,
-// including data structures for handling encrypted data and their JSON serialization.
-
-package symmetric
